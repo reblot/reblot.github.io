9d69f9bedd0f85471ae91934f4d5cfa9d0055c54
reblot <anbu.dev@yahoo.com>
2020-01-07 02:00:10 +0000

    update

--- sw.js
+++ sw.js
@@ -18,13 +18,8 @@ self.addEventListener('install', async event => {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     }).catch(function() {
         return caches.match('./offline.html');
     })
